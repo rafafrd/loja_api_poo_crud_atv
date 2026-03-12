f5b59a9dab81532bd4efc3333ab11a3a6ed0129c
rafafrd <bolekao03@gmail.com>
2026-03-12 10:38:16 -0300

    add multer

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-
+import categoriaRoutes from "./categoria.routes";
 
 const router = Router();
 
+router.use("/categorias", categoriaRoutes);
 
 
 export default router;
